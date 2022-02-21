b549b0af550aac08693936f5bdca4652fd34d59a
AngAlRo <angel.al71@protonmail.com>
2022-02-21 19:39:30 +0100

    result code of index.js

--- src/for and if/exercise9/index.js
+++ src/for and if/exercise9/index.js
@@ -1,7 +1,14 @@
 let numbers = [0,1,2,3,4,5,6,7,8,9]
 
 for ( let left of numbers) {
+    if (left > 0){
+        console.log("\n" + "Tabla del "+left+"\n");
+    }
     for (let right of numbers) {
-        // Put your code here
+        if (left > 0){
+            if (right > 0){
+                console.log(left + " x " +  right + " = " + left * right);
+            }
+        }
     }
 }
